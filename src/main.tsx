78ab1b3d4c772b68b076b030eff00cd9c9ca491c
jayzone91 <register@j4yz0n3.de>
2025-03-04 16:48:59 +0100

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
+import "./index.css";
+import Home from "./Pages/Home";
+import RootLayout from "./RootLayout";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
